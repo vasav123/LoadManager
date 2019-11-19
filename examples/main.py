26b7205c4a5d807a9b33a3250e4168455559857f
vasav123 <vasav.shah97@gmail.com>
2019-11-18 21:21:05 -0500

    made the plot chaning with time

--- examples/main.py
+++ examples/main.py
@@ -11,7 +11,10 @@ import numpy as np
 class TestWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(TestWindow, self).__init__()
+        self.timer = QtCore.QTimer(self)
+
         self.ui = example.Ui_MainWindow() # in this and next line you say that you will use all widgets from testUI over self.ui
+
         self.page2 = QtWidgets.QWidget()
         self.pg2 = page2.Ui_widget()
         self.pg2.setupUi(self.page2)
@@ -29,15 +32,23 @@ class TestWindow(QtWidgets.QMainWindow):
         self.pg2.Home.clicked.connect(self.goHome);
         self.pg3.pushButton.clicked.connect(self.goHome)
 
-        self.pg3.graphicsView.plot(np.random.normal(size = 100),np.random.normal(size = 100),pen = None, symbol = 'o')
+        
 
     def DoSomething(self):
         self.ui.stackedWidget.setCurrentIndex(2)
+        
+        self.timer.setInterval(100)
+        self.timer.start()
+        self.timer.timeout.connect(self.onNewData)
         print(self.ui.stackedWidget.count())
 
     def goHome(self):
         self.ui.stackedWidget.setCurrentIndex(0)
-        
+        self.timer.stop()
+
+    def onNewData(self):
+        self.pg3.graphicsView.plot(np.random.normal(size = 100),np.random.normal(size = 100),clear=True)
+        print("timer on going")
 
         # self.gridLayout.addWidget(self.pushButton_4, 4, 0, 1, 1)
 if __name__ == '__main__':
