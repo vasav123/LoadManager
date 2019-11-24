afd05b3041d6ec483f22903a1a3a79457bcec152
vasav123 <vasav.shah97@gmail.com>
2019-11-23 19:52:23 -0500

    mixed the mainWidgetWrapper with graph and playerStats

--- GUI/graphs.py
+++ GUI/graphs.py
@@ -10,51 +10,51 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(566, 510)
-        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
+class Ui_graphs(object):
+    def setupUi(self, graphs):
+        graphs.setObjectName("graphs")
+        graphs.resize(566, 510)
+        self.verticalLayout = QtWidgets.QVBoxLayout(graphs)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.graphicsVie = PlotWidget(Form)
-        self.graphicsVie.setObjectName("graphicsVie")
-        self.verticalLayout.addWidget(self.graphicsVie)
+        self.graphicsView = PlotWidget(graphs)
+        self.graphicsView.setObjectName("graphicsView")
+        self.verticalLayout.addWidget(self.graphicsView)
         spacerItem = QtWidgets.QSpacerItem(20, 10, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Preferred)
         self.verticalLayout.addItem(spacerItem)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.accel = QtWidgets.QPushButton(Form)
+        self.accel = QtWidgets.QPushButton(graphs)
         self.accel.setObjectName("accel")
         self.horizontalLayout.addWidget(self.accel)
-        self.gyro = QtWidgets.QPushButton(Form)
+        self.gyro = QtWidgets.QPushButton(graphs)
         self.gyro.setObjectName("gyro")
         self.horizontalLayout.addWidget(self.gyro)
-        self.mag = QtWidgets.QPushButton(Form)
+        self.mag = QtWidgets.QPushButton(graphs)
         self.mag.setObjectName("mag")
         self.horizontalLayout.addWidget(self.mag)
-        self.pressure = QtWidgets.QPushButton(Form)
+        self.pressure = QtWidgets.QPushButton(graphs)
         self.pressure.setObjectName("pressure")
         self.horizontalLayout.addWidget(self.pressure)
         self.verticalLayout.addLayout(self.horizontalLayout)
 
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
+        self.retranslateUi(graphs)
+        QtCore.QMetaObject.connectSlotsByName(graphs)
 
-    def retranslateUi(self, Form):
+    def retranslateUi(self, graphs):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
-        self.accel.setText(_translate("Form", "Accelormeter"))
-        self.gyro.setText(_translate("Form", "Gyroscope"))
-        self.mag.setText(_translate("Form", "Magnetometer"))
-        self.pressure.setText(_translate("Form", "Pressure"))
+        graphs.setWindowTitle(_translate("graphs", "Form"))
+        self.accel.setText(_translate("graphs", "Accelormeter"))
+        self.gyro.setText(_translate("graphs", "Gyroscope"))
+        self.mag.setText(_translate("graphs", "Magnetometer"))
+        self.pressure.setText(_translate("graphs", "Pressure"))
 from pyqtgraph import PlotWidget
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = Ui_Form()
-    ui.setupUi(Form)
-    Form.show()
+    graphs = QtWidgets.QWidget()
+    ui = Ui_graphs()
+    ui.setupUi(graphs)
+    graphs.show()
     sys.exit(app.exec_())
---
+++ GUI/mainWidgetWrapper.py
@@ -0,0 +1,36 @@
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import mainWidget
+import playerStats
+import graphs
+
+class mainWidgetWrapper(mainWidget.Ui_Form):
+	def setupUi(self,Form):
+		mainWidget.Ui_Form.setupUi(self,Form)
+		self.pStats = QtWidgets.QWidget()
+		self.pStats_widget = playerStats.Ui_playerStats()
+		self.pStats_widget.setupUi(self.pStats)
+		
+		self.hbox = QtGui.QHBoxLayout()
+		self.hbox.addWidget(self.pStats)
+
+		self.playerStats.setLayout(self.hbox)
+
+		self.gStats = QtWidgets.QWidget()
+		self.graph_widget = graphs.Ui_graphs()
+		self.graph_widget.setupUi(self.gStats)
+
+		self.hbox2 = QtGui.QHBoxLayout()
+		self.hbox2.addWidget(self.gStats)
+
+		self.graphs.setLayout(self.hbox2)
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = mainWidgetWrapper()
+    ui.setupUi(Form)
+    Form.show()
+    # Form.setStyleSheet(open('test.qss').read())
+    sys.exit(app.exec_())
