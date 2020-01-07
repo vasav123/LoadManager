f13b23bce21a9cdfeff5024e355175aece0572d3
vasav123 <vasav.shah97@gmail.com>
2020-01-07 15:26:08 -0500

    added JSON library, did basic wifi, and added mqtt library

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,28 +1,89 @@
 #include <Wire.h>
 #include <SPI.h>
+
+#include <WiFi.h>
+#include "PubSubClient.h"
+#include "ArduinoJson.h"
+
 #include <SparkFunLSM9DS1.h>
+
+
+/* Defs for network connfiguration and MQTT Broker */
+const char* ssid     = "Vasav-Computer";
+const char* password = "6fQBxWeq";
+const char* mqtt_server = "10.42.0.1";
+const char* nodeId = "123123123";
+int port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 LSM9DS1 imu;
 
 #define PRINT_CALCULATED
 #define PRINT_SPEED 250 // 250 ms between prints
 static unsigned long lastPrint = 0; // Keep track of print time
+const char* publishNode = "loadmanager";
 
-// Earth's magnetic field varies by location. Add or subtract
-// a declination to get a more accurate heading. Calculate
-// your's here:
 // http://www.ngdc.noaa.gov/geomag-web/#declination
-#define DECLINATION 9.92 // Declination (degrees) in Boulder, CO.
+#define DECLINATION 9.92 // Declination (degrees) in Hamilton Ontario.
 
 //Function definitions
 void printGyro();
 void printAccel();
 void printMag();
-void printAttitude(float ax, float ay, float az, float mx, float my, float mz);
+void printAttitude(float ax, float ay, float az, float mx, float my, float mz, float *heading, float *pitch, float *roll);
+
+/* Setup the WiFi Connection when required */
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  int timeout=0;
+  while (WiFi.status() != WL_CONNECTED && timeout++<100) {
+    delay(500);
+    Serial.print(".");
+  }
+  if(timeout<100){
+    randomSeed(micros());
+  
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  }else{
+    Serial.println("wifi not connected!");
+  }
+}
+
+/* reconnection */
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "SensorNode-";
+    clientId += nodeId;
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
 
 void setup()
 {
   Serial.begin(115200);
+
+  setup_wifi();
+  client.setServer(mqtt_server, port);
 
   Wire.begin();
 
@@ -40,42 +101,44 @@ void setup()
 
 void loop()
 {
-  // Update the sensor values whenever new data is available
+  if (WiFi.status() == WL_CONNECTED){
+    if (!client.connected()) {
+      reconnect();
+    }
+    client.loop();
+    
+    long now = millis();
+//    Serial.print("Publish message: ");
+//    Serial.println(msg);
+    client.publish(publishNode, "hello world");
+//  WiFi.disconnect();
+  }else{
+    Serial.println("wifi not connected going to restart in 5 mins!");
+  }
   if ( imu.gyroAvailable() )
   {
-    // To read from the gyroscope,  first call the
-    // readGyro() function. When it exits, it'll update the
-    // gx, gy, and gz variables with the most current data.
     imu.readGyro();
   }
   if ( imu.accelAvailable() )
   {
-    // To read from the accelerometer, first call the
-    // readAccel() function. When it exits, it'll update the
-    // ax, ay, and az variables with the most current data.
     imu.readAccel();
   }
   if ( imu.magAvailable() )
   {
-    // To read from the magnetometer, first call the
-    // readMag() function. When it exits, it'll update the
-    // mx, my, and mz variables with the most current data.
     imu.readMag();
   }
 
   if ((lastPrint + PRINT_SPEED) < millis())
   {
+    float heading, pitch, roll;
     printGyro();  // Print "G: gx, gy, gz"
     printAccel(); // Print "A: ax, ay, az"
     printMag();   // Print "M: mx, my, mz"
-    // Print the heading and orientation for fun!
-    // Call print attitude. The LSM9DS1's mag x and y
-    // axes are opposite to the accelerometer, so my, mx are
-    // substituted for each other.
     printAttitude(imu.ax, imu.ay, imu.az,
-                  -imu.my, -imu.mx, imu.mz);
-    Serial.println();
+                  -imu.my, -imu.mx, imu.mz,
+                   &heading, &pitch, &roll);
 
+    
     lastPrint = millis(); // Update lastPrint time
   }
 }
@@ -158,30 +221,24 @@ void printMag()
 // http://cache.freescale.com/files/sensors/doc/app_note/AN3461.pdf?fpsp=1
 // Heading calculations taken from this app note:
 // http://www51.honeywell.com/aero/common/documents/myaerospacecatalog-documents/Defense_Brochures-documents/Magnetic__Literature_Application_notes-documents/AN203_Compass_Heading_Using_Magnetometers.pdf
-void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
+void printAttitude(float ax, float ay, float az, float mx, float my, float mz, float *heading, float *pitch, float *roll)
 {
-  float roll = atan2(ay, az);
-  float pitch = atan2(-ax, sqrt(ay * ay + az * az));
+  *roll = atan2(ay, az);
+  *pitch = atan2(-ax, sqrt(ay * ay + az * az));
 
-  float heading;
+//  float heading;
   if (my == 0)
-    heading = (mx < 0) ? PI : 0;
+    *heading = (mx < 0) ? PI : 0;
   else
-    heading = atan2(mx, my);
+    *heading = atan2(mx, my);
 
-  heading -= DECLINATION * PI / 180;
+  *heading -= DECLINATION * PI / 180;
 
-  if (heading > PI) heading -= (2 * PI);
-  else if (heading < -PI) heading += (2 * PI);
+  if (*heading > PI) *heading -= (2 * PI);
+  else if (*heading < -PI) *heading += (2 * PI);
 
   // Convert everything from radians to degrees:
-  heading *= 180.0 / PI;
-  pitch *= 180.0 / PI;
-  roll  *= 180.0 / PI;
-
-  Serial.print("Pitch, Roll: ");
-  Serial.print(pitch, 2);
-  Serial.print(", ");
-  Serial.println(roll, 2);
-  Serial.print("Heading: "); Serial.println(heading, 2);
+  *heading *= 180.0 / PI;
+  *pitch *= 180.0 / PI;
+  *roll  *= 180.0 / PI;
 }
