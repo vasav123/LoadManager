cf26e43806632035efba38d2ef96fd268c4f999b
vasav123 <vasav.shah97@gmail.com>
2020-01-08 15:31:50 -0500

    got the mqtt/wifi stuff working with Vasav's computer, code still needs tog et cleaned up

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,6 +1,5 @@
 #include <Wire.h>
 #include <SPI.h>
-
 #include <WiFi.h>
 #include "PubSubClient.h"
 #include "ArduinoJson.h"
@@ -14,6 +13,7 @@ const char* password = "6fQBxWeq";
 const char* mqtt_server = "10.42.0.1";
 const char* nodeId = "123123123";
 int port = 1883;
+int packet_num = 0;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -22,8 +22,14 @@ LSM9DS1 imu;
 
 #define PRINT_CALCULATED
 #define PRINT_SPEED 250 // 250 ms between prints
+int count = 0;
 static unsigned long lastPrint = 0; // Keep track of print time
 const char* publishNode = "loadmanager";
+
+StaticJsonDocument<1000> packet;
+char msg[2000];
+
+float temp_array[4];
 
 // http://www.ngdc.noaa.gov/geomag-web/#declination
 #define DECLINATION 9.92 // Declination (degrees) in Hamilton Ontario.
@@ -33,7 +39,7 @@ void printGyro();
 void printAccel();
 void printMag();
 void printAttitude(float ax, float ay, float az, float mx, float my, float mz, float *heading, float *pitch, float *roll);
-
+float getMagnitude(float x, float y, float z);
 /* Setup the WiFi Connection when required */
 void setup_wifi() {
 
@@ -42,7 +48,27 @@ void setup_wifi() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-
+  packet.createNestedArray("accel_top");
+  copyArray(temp_array, 4, packet["accel_top"]);
+  packet.createNestedArray("accel_bot");
+  copyArray(temp_array, 4, packet["accel_bot"]);
+  packet.createNestedArray("fsr_quad");
+  copyArray(temp_array, 4, packet["fsr_quad"]);
+  packet.createNestedArray("fsr_ham");
+  copyArray(temp_array, 4, packet["fsr_ham"]);
+  packet.createNestedArray("yaw_top");
+  copyArray(temp_array, 4, packet["yaw_top"]);
+  packet.createNestedArray("pitch_top");
+  copyArray(temp_array, 4, packet["pitch_top"]);
+  packet.createNestedArray("roll_top");
+  copyArray(temp_array, 4, packet["roll_top"]);
+  packet.createNestedArray("yaw_bot");
+  copyArray(temp_array, 4, packet["yaw_bot"]);
+  packet.createNestedArray("pitch_bot");
+  copyArray(temp_array, 4, packet["pitch_bot"]);
+  packet.createNestedArray("roll_bot");
+  copyArray(temp_array, 4, packet["roll_bot"]);
+   
   WiFi.begin(ssid, password);
   int timeout=0;
   while (WiFi.status() != WL_CONNECTED && timeout++<100) {
@@ -86,7 +112,6 @@ void setup()
   client.setServer(mqtt_server, port);
 
   Wire.begin();
-
   if (imu.begin() == false) // with no arguments, this uses default addresses (AG:0x6B, M:0x1E) and i2c port (Wire).
   {
     Serial.println("Failed to communicate with LSM9DS1.");
@@ -101,20 +126,6 @@ void setup()
 
 void loop()
 {
-  if (WiFi.status() == WL_CONNECTED){
-    if (!client.connected()) {
-      reconnect();
-    }
-    client.loop();
-    
-    long now = millis();
-//    Serial.print("Publish message: ");
-//    Serial.println(msg);
-    client.publish(publishNode, "hello world");
-//  WiFi.disconnect();
-  }else{
-    Serial.println("wifi not connected going to restart in 5 mins!");
-  }
   if ( imu.gyroAvailable() )
   {
     imu.readGyro();
@@ -130,19 +141,55 @@ void loop()
 
   if ((lastPrint + PRINT_SPEED) < millis())
   {
-    float heading, pitch, roll;
-    printGyro();  // Print "G: gx, gy, gz"
-    printAccel(); // Print "A: ax, ay, az"
-    printMag();   // Print "M: mx, my, mz"
+    float heading =0 , pitch = 0 , roll = 0;
+//    printGyro();  // Print "G: gx, gy, gz"
+//    printAccel(); // Print "A: ax, ay, az"
+//    printMag();   // Print "M: mx, my, mz"
     printAttitude(imu.ax, imu.ay, imu.az,
                   -imu.my, -imu.mx, imu.mz,
                    &heading, &pitch, &roll);
-
+   
+    packet["pitch_top"][count] = pitch;
+    packet["pitch_bot"][count] = 0;
+    packet["yaw_top"][count] = heading;
+    packet["yaw_bot"][count] = 0;
+    packet["roll_top"][count] = roll;
+    packet["roll_bot"][count] = 0;
+    packet["accel_top"][count] = getMagnitude(imu.ax, imu.ay, imu.az);
+    packet["accel_bot"][count] = 0;
     
+    count++;
+    serializeJson(packet, msg);
     lastPrint = millis(); // Update lastPrint time
+    Serial.println(pitch);
+    
+    if (count == 4){
+        packet_num++;
+        packet["packet_num"] = packet_num;
+        count = 0;
+        if (WiFi.status() == WL_CONNECTED){
+          if (!client.connected()) {
+            reconnect();
+          }
+          client.loop();
+          long now = millis();
+          client.publish(publishNode, msg);
+  //        Serial.println(msg);
+  //        delete &packet;
+
+          packet["size"] = 4;
+//          packet.remove("pitch_top");
+      }else{
+        Serial.println("wifi not connected");
+      }
+    }
   }
 }
 
+float getMagnitude(float x, float y, float z)
+{
+  return sqrt(pow(x,2) + pow(y,2) + pow(z,2));
+}
 void printGyro()
 {
   // Now we can use the gx, gy, and gz variables as we please.
