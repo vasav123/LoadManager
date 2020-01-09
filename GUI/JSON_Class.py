d736b9c520657b4a80f03f424735bd265b92ec9b
michaelk29 <michaelk29@users.noreply.github.com>
2020-01-09 13:37:26 -0500

    Elminated Error

--- GUI/JSON_Class.py
+++ GUI/JSON_Class.py
@@ -16,7 +16,7 @@ class packet:
         self.roll_bot = packet['roll_bot']
     
     
-if __name__ = "__main__":    
+if __name__ == "__main__":    
     x =  '{ "packet_num":50,"size":10, "accel_top":[1.0,2.2,3.3,4.4,5.5,6.5,7.4,8.2,9.8,10.1],\
     "accel_bot":[1.0,2.2,3.3,4.4,5.5,6.5,7.4,8.2,9.8,10.1],"fsr_quad":50, "fsr_ham":2, "yaw_top":1, "pitch_top":[11,13], "roll_top":13,\
     "yaw_bot":45, "pitch_bot":20, "roll_bot":33,"yaw_top":"New York" }'
