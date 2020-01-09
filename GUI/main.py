f13f467634c00c37a137c82fe4a3fd24958e7eaa
michaelk29 <michaelk29@users.noreply.github.com>
2020-01-09 15:00:14 -0500

    Update main.py

--- GUI/main.py
+++ GUI/main.py
@@ -2,9 +2,11 @@ import sys
 from PyQt5 import   QtGui, QtCore, QtWidgets
 import mainWindow
 from  mainWidgetWrapper import *
-import numpy as np 
+import numpy as np
+import threading
+from collectData import *
 
-
+mqtt_client = None
 
 class TestWindow():
     def __init__(self):
@@ -22,8 +24,22 @@ class TestWindow():
         self.statsWidget_obj.back.clicked.connect(lambda:self.ui.appWidgets.setCurrentIndex(0))
         self.ui.kawhiButton.clicked.connect(lambda:self.ui.appWidgets.setCurrentIndex(1))
 
+
+def pushOnQ():
+    mqtt_client = collectData()
+    rc = 0
+    while rc == 0:
+        rc=test.loop()
+
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = TestWindow()
+
+    
+
+    pushThread=threading.Thread(target=pushOnQ)
+    pushThread.start()
+    
 
     sys.exit(app.exec_())
