c319a1c75be4bc5bc95d5088f100f7a05a052cb7
vasav123 <vasav.shah97@gmail.com>
2020-02-27 15:31:53 -0500

    udpated the fiels based of new pins and lowered the polling time

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,5 +1,3 @@
-#include <dummy.h>
-
 #include <Wire.h>
 #include <SPI.h>
 #include <WiFi.h>
@@ -14,7 +12,8 @@ const char* mqtt_server = "192.168.137.1";//Vasav- "10.42.0.1", Michael, 192.168
 const char* nodeId = "123123123";
 int port = 1883;
 int packet_num = 0;
-
+int cs_1 = 9;
+int cs_2 = 10; // need to change this
 WiFiClient espClient;
 PubSubClient client(espClient);
 
@@ -23,7 +22,7 @@ LSM9DS1 imu2;
 
 
 #define PRINT_CALCULATED
-#define PRINT_SPEED 250 // 250 ms between prints
+#define PRINT_SPEED 5 // 250 ms between prints
 int count = 0;
 static unsigned long lastPrint = 0; // Keep track of print time
 const char* publishNode = "loadmanager";
@@ -38,7 +37,7 @@ float temp_array[4];
 
 void printAttitude(float ax, float ay, float az, float mx, float my, float mz, float *heading, float *pitch, float *roll);
 float getMagnitude(float x, float y, float z);
-
+unsigned int readFSR(int fsr);
 void setup_wifi() {
 
   delay(10);
@@ -108,7 +107,11 @@ void setup()
   setup_wifi();
   client.setServer(mqtt_server, port);
   Wire.begin();
-  Wire1.begin(23,18,100000); //23 clk 18 data
+  Wire1.begin(13,14,100000); //23 clk 25 data
+  SPI.begin();
+  pinMode(cs_1, OUTPUT);
+  pinMode(cs_2, OUTPUT);
+
   if (imu.begin() == false) // with no arguments, this uses default addresses (AG:0x6B, M:0x1E) and i2c port (Wire).
   {
     Serial.println("Failed to communicate with LSM9DS1.");
@@ -121,18 +124,18 @@ void setup()
   }else{
     Serial.println("IMU1 connected!");
   }
-  if (imu2.begin( LSM9DS1_AG_ADDR(1), LSM9DS1_M_ADDR(1), Wire1) == false) // with no arguments, this uses default addresses (AG:0x6B, M:0x1E) and i2c port (Wire).
-  {
-    Serial.println("Failed to communicate with LSM9DS1.");
-    Serial.println("Double-check wiring.");
-    Serial.println("Default settings in this sketch will " \
-                   "work for an out of the box LSM9DS1 " \
-                   "Breakout, but may need to be modified " \
-                   "if the board jumpers are.");
-    while (1);
-  }else{
-    Serial.println("IMU2 connected!");
-  }
+ if (imu2.begin( LSM9DS1_AG_ADDR(1), LSM9DS1_M_ADDR(1), Wire1) == false) // with no arguments, this uses default addresses (AG:0x6B, M:0x1E) and i2c port (Wire).
+ {
+   Serial.println("Failed to communicate with LSM9DS1.");
+   Serial.println("Double-check wiring.");
+   Serial.println("Default settings in this sketch will " \
+                  "work for an out of the box LSM9DS1 " \
+                  "Breakout, but may need to be modified " \
+                  "if the board jumpers are.");
+   while (1);
+ }else{
+   Serial.println("IMU2 connected!");
+ }
 }
 
 void loop()
@@ -149,27 +152,27 @@ void loop()
   {
     imu.readMag();
   }
-  if ( imu2.gyroAvailable() )
-  {
-    imu2.readGyro();
-  }
-  if ( imu2.accelAvailable() )
-  {
-    imu2.readAccel();
-  }
-  if ( imu2.magAvailable() )
-  {
-    imu2.readMag();
-  }
+ if ( imu2.gyroAvailable() )
+ {
+   imu2.readGyro();
+ }
+ if ( imu2.accelAvailable() )
+ {
+   imu2.readAccel();
+ }
+ if ( imu2.magAvailable() )
+ {
+   imu2.readMag();
+ }
   if ((lastPrint + PRINT_SPEED) < millis())
   {
     float heading =0 , pitch = 0 , roll = 0, heading2 = 0, pitch2 = 0, roll2 = 0;
     calcAttitude(imu.ax, imu.ay, imu.az,
                   -imu.my, -imu.mx, imu.mz,
                    &heading, &pitch, &roll);
-    calcAttitude(imu2.ax, imu2.ay, imu2.az,
-                  -imu2.my, -imu2.mx, imu2.mz,
-                   &heading2, &pitch2, &roll2);
+   calcAttitude(imu2.ax, imu2.ay, imu2.az,
+                 -imu2.my, -imu2.mx, imu2.mz,
+                  &heading2, &pitch2, &roll2);
    
     packet["pitch_top"][count] = pitch;
     packet["pitch_bot"][count] = pitch2;
@@ -179,13 +182,13 @@ void loop()
     packet["roll_bot"][count] = roll2;
     packet["accel_top"][count] = getMagnitude(imu.calcAccel(imu.ax), imu.calcAccel(imu.ay), imu.calcAccel(imu.az));
     packet["accel_bot"][count] = getMagnitude(imu.calcAccel(imu2.ax), imu.calcAccel(imu2.ay), imu.calcAccel(imu2.az));
-    packet["fsr_quad"][count] = analogRead(33);
-    packet["fsr_ham"][count] = 0;
+    packet["fsr_quad"][count] = readFSR(0);
+    packet["fsr_ham"][count] = readFSR(1);
     count++;
     serializeJson(packet, msg);
     lastPrint = millis(); // Update lastPrint time
     
-//    Serial.println(pitch);
+//    Serial.println(msg);
     
     if (count == 4){
         packet_num++;
@@ -230,4 +233,25 @@ void calcAttitude(float ax, float ay, float az, float mx, float my, float mz, fl
   *heading *= 180.0 / PI;
   *pitch *= 180.0 / PI;
   *roll  *= 180.0 / PI;
+}
+
+unsigned int readFSR(int fsr){
+  if (fsr == 0){
+    digitalWrite(cs_1, LOW);
+  }else{
+    digitalWrite(cs_2, LOW);
+  }
+  SPI.beginTransaction(SPISettings(125000, MSBFIRST, SPI_MODE1));
+  unsigned int fsr_value = 0;
+  fsr_value = SPI.transfer16(0x00);
+  fsr_value = fsr_value& 0b111111111111;
+  fsr_value >> 1;
+
+  if (fsr == 0){
+    digitalWrite(cs_1, HIGH);
+  }else{
+    digitalWrite(cs_2, HIGH);
+  }
+  SPI.endTransaction();
+  return fsr_value;
 }
